b1050341585c11049eb1c07063b0f49e8873e4d9
Mavarok <main0513sam@yandex.ru>
2025-07-07 15:52:01 +0300

    Fix working with window context. Make window active before loading OpenGL and add calling of window display method in cicle. Also change condithion for while-cicle to bool variable and now window close after cicle end

--- HelloOpenGL/main.cpp
+++ HelloOpenGL/main.cpp
@@ -9,7 +9,8 @@ int main() {
 	unsigned window_height = 480u;
 
 	sf::Window window(sf::VideoMode(sf::Vector2u{ window_width, window_height }), L"Тест", sf::Style::Titlebar | sf::Style::Close);
-	
+	window.setActive(true);
+
 	if (!gladLoadGL()) {
 		std::wcerr << L"Failed to initialize GLAD" << std::endl;
 		return -1;
@@ -17,18 +18,24 @@ int main() {
 
 	glViewport(0, 0, window.getSize().x, window.getSize().y);
 
-	while (window.isOpen()) {
+	bool is_running = true;
+	while (is_running) {
 		while (const std::optional event = window.pollEvent()) {
 			if (event->is<sf::Event::Closed>()) {
-				window.close();
+				is_running = false;
 			} else if (event->is<sf::Event::Resized>()) {
 				auto* const resized = event->getIf<sf::Event::Resized>();
 				glViewport(0, 0, resized->size.x, resized->size.y);
 			}
 		}
 
+		glClearColor(.02f, .75f, .23f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
+		window.display();
 	}
+
+	window.close();
 
 	return 0;
 }
