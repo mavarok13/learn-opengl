d8aaa2023fed8b442778d16ed337a4e114360fea
Mavarok <main0513sam@yandex.ru>
2025-07-17 16:10:15 +0300

    Remove HelloOpenGL-exercise0-4-1

--- HelloOpenGL-exercises/exercise1.cpp
+++ HelloOpenGL-exercises/exercise1.cpp
@@ -11,8 +11,8 @@
 #include "src/shader_loader.hpp"
 
 // * FIX THIS
-const char* vertex_shader_path = "D:/dev/source/learn-opengl/HelloOpenGL-exercise0-4-1/src/vertex.glsl";
-const char* fragment_shader_path = "D:/dev/source/learn-opengl/HelloOpenGL-exercise0-4-1/src/fragment.glsl";
+const char* vertex_shader_path = "D:/dev/source/learn-opengl/HelloOpenGL/src/vertex.glsl";
+const char* fragment_shader_path = "D:/dev/source/learn-opengl/HelloOpenGL/src/fragment.glsl";
 
 int main() {
 	unsigned window_width = 720u;
