fca6bbc4500623f176875e377cae3e7a7ba0c0fd
Mavarok <main0513sam@yandex.ru>
2025-07-19 19:00:55 +0300

    Change colorfulshape.cpp to work with shader_loader::ShaderProgram

--- HelloOpenGL2/colorfulshape.cpp
+++ HelloOpenGL2/colorfulshape.cpp
@@ -26,59 +26,7 @@ int main() {
 
 	glViewport(0, 0, window.getSize().x, window.getSize().y);
 
-// * Compile vertex shader
-	int success; char info_log[512];
-
-	std::string vertex_shader_source_code = shader_loader::ReadShaderSourceFromFile(vertex_shader_path);
-	const char* vertex_shader_source_code_c_str = vertex_shader_source_code.c_str();
-
-	unsigned vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, &vertex_shader_source_code_c_str, NULL);
-	std::cout << "[SHADER COMPILE INFO]: Start compile vertex shader..." << std::endl;
-	glCompileShader(vertex_shader);
-
-	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
-		std::cerr << "[SHADER COMPILE ERR]: " << info_log << std::endl;
-	} else {
-		std::cout << "[SHADER COMPILE INFO]: Shader compile successfuly" << std::endl;
-	}
-
-// * Compile fragment shader
-	std::string fragment_shader_source_code = shader_loader::ReadShaderSourceFromFile(fragment_shader_path);
-	const char* fragment_shader_source_code_c_str = fragment_shader_source_code.c_str();
-
-	unsigned fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, &fragment_shader_source_code_c_str, NULL);
-	std::cout << "[SHADER COMPILE INFO]: Start compile fragment shader..." << std::endl;
-	glCompileShader(fragment_shader);
-
-	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
-		std::cerr << "[SHADER COMPILE ERR]: " << info_log << std::endl;
-	} else {
-		std::cout << "[SHADER COMPILE INFO]: Shader compile successfuly" << std::endl;
-	}
-
-// * Link shaders
-// * First program
-	unsigned shader_program = glCreateProgram();
-	glAttachShader(shader_program, vertex_shader);
-	glAttachShader(shader_program, fragment_shader);
-	glLinkProgram(shader_program);
-
-	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-
-	if (!success) {
-		glGetProgramInfoLog(shader_program, 512, NULL, info_log);
-		std::cerr << "[SHADER LINK ERR]: " << info_log << std::endl;
-	} else {
-		std::cout << "[SHADER LINK INFO]: Shader link successfuly" << std::endl;
-	}
+	shader_loader::ShaderProgram shader_program(vertex_shader_path, fragment_shader_path);
 
 // * ======================
 
@@ -126,7 +74,7 @@ int main() {
 		glClearColor(.0f, .0f, .0f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shader_program);
+		shader_program.Use();
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
