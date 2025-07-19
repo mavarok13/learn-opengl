0df250e3cbfc830d7f6c7de88a756b8422b47b3f
Mavarok <main0513sam@yandex.ru>
2025-07-19 18:57:20 +0300

    Change main.cpp to work with shader_loader::ShaderProgram class

--- HelloOpenGL2/main.cpp
+++ HelloOpenGL2/main.cpp
@@ -27,95 +27,9 @@ int main() {
 
 	glViewport(0, 0, window.getSize().x, window.getSize().y);
 
-// * Compile vertex shader
-	int success; char info_log[512];
-
-	std::string vertex_shader_source_code = shader_loader::ReadShaderSourceFromFile(vertex_shader_path);
-	const char* vertex_shader_source_code_c_str = vertex_shader_source_code.c_str();
-
-	unsigned vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, &vertex_shader_source_code_c_str, NULL);
-	std::cout << "[SHADER COMPILE INFO]: Start compile vertex shader..." << std::endl;
-	glCompileShader(vertex_shader);
-
-	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
-		std::cerr << "[SHADER COMPILE ERR]: " << info_log << std::endl;
-	} else {
-		std::cout << "[SHADER COMPILE INFO]: Shader compile successfuly" << std::endl;
-	}
-
-// * Compile 1st fragment shader
-	std::string fragment_shader_source_code = shader_loader::ReadShaderSourceFromFile(fragment_shader_path);
-	const char* fragment_shader_source_code_c_str = fragment_shader_source_code.c_str();
-
-	unsigned fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, &fragment_shader_source_code_c_str, NULL);
-	std::cout << "[SHADER COMPILE INFO]: Start compile fragment shader..." << std::endl;
-	glCompileShader(fragment_shader);
-
-	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
-		std::cerr << "[SHADER COMPILE ERR]: " << info_log << std::endl;
-	} else {
-		std::cout << "[SHADER COMPILE INFO]: Shader compile successfuly" << std::endl;
-	}
-
-// * Compile 2nd fragment shader
-	std::string fragment2_shader_source_code = shader_loader::ReadShaderSourceFromFile(fragment2_shader_path);
-	const char* fragment2_shader_source_code_c_str = fragment2_shader_source_code.c_str();
-
-	unsigned fragment_shader2 = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader2, 1, &fragment2_shader_source_code_c_str, NULL);
-
-	std::cout << "[SHADER COMPILE INFO]: Starting compile fragment shader..." << std::endl;
-	glCompileShader(fragment_shader2);
-	glGetShaderiv(fragment_shader2, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		glGetShaderInfoLog(fragment_shader2, 512, NULL, info_log);
-		std::cerr << "[SHADER COMPILE ERR]: " << info_log << std::endl;
-	} else {
-		std::cout << "[SHADER COMPILE INFO]: Shader compile successfuly" << std::endl;
-	}
-
-// * Link shaders
-// * First program
-	unsigned shader_program = glCreateProgram();
-	glAttachShader(shader_program, vertex_shader);
-	glAttachShader(shader_program, fragment_shader);
-	glLinkProgram(shader_program);
-
-	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-
-	if (!success) {
-		glGetProgramInfoLog(shader_program, 512, NULL, info_log);
-		std::cerr << "[SHADER LINK ERR]: " << info_log << std::endl;
-	} else {
-		std::cout << "[SHADER LINK INFO]: Shader link successfuly" << std::endl;
-	}
-
-// * Second program
-	unsigned shader_program2 = glCreateProgram();
-	glAttachShader(shader_program2, vertex_shader);
-	glAttachShader(shader_program2, fragment_shader2);
-	glLinkProgram(shader_program2);
-
-	glGetProgramiv(shader_program2, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shader_program2, 512, NULL, info_log);
-		std::cerr << "[SHADER LINK ERR]: " << info_log << std::endl;
-	} else {
-		std::cout << "[SHADER LINK INFO]: Shader link successfuly" << std::endl;
-	}
-
-	glDeleteShader(vertex_shader);
-	glDeleteShader(fragment_shader);
-	glDeleteShader(fragment_shader2);
+// * Create program
+	shader_loader::ShaderProgram shader_program1(vertex_shader_path, fragment_shader_path);
+	shader_loader::ShaderProgram shader_program2(vertex_shader_path, fragment2_shader_path);
 
 // * ======================
 
@@ -165,11 +79,6 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-// * Get uniform variables
-
-	unsigned color1 = glGetUniformLocation(shader_program, "color");
-	unsigned color2 = glGetUniformLocation(shader_program2, "color");
-
 // * Create clock
 	std::chrono::steady_clock sc;
 	auto now = sc.now();
@@ -190,15 +99,15 @@ int main() {
 		glClearColor(.0f, .0f, .0f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shader_program);
 		float red_color = std::sin(.00000001f*(sc.now() - now).count())+.5f;
-		glUniform4f(color1, red_color, .0f, .0f, 1.f);
+		shader_program1.SetUniform4f("color", red_color, .0f, .0f, 1.f);
+		shader_program1.Use();
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		glUseProgram(shader_program2);
 		float blue_color = std::cos(.00000001f*(sc.now() - now).count()+3.14f/2)+.5f;
-		glUniform4f(color1, .0f, .0f, blue_color, 1.f);
+		shader_program2.SetUniform4f("color", .0f, .0f, blue_color, .0f);
+		shader_program2.Use();
 		glBindVertexArray(VAO2);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
